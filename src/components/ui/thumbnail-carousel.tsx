28e63a61bc262c6890e1d9b558f3e662b32fe375
[dyad] <khadeem.smart@outlook.com>
2025-11-15 02:48:29 +0100

    [dyad] Removing image background and click - wrote 1 file(s)

--- src/components/ui/thumbnail-carousel.tsx
+++ src/components/ui/thumbnail-carousel.tsx
@@ -1,10 +1,5 @@
 import React from 'react';
 import Hotspot from './Hotspot';
-import {
-  Dialog,
-  DialogContent,
-  DialogTrigger,
-} from "@/components/ui/dialog";
 
 interface HotspotData {
   x: number;
@@ -35,36 +30,25 @@ const item: CarouselItem = {
 export default function ThumbnailCarousel() {
   return (
     <div className='w-full max-w-3xl mx-auto p-4 lg:p-10'>
-      <Dialog>
-        <DialogTrigger asChild>
-          <div className='relative overflow-hidden rounded-lg bg-gray-100 cursor-pointer group'>
-            <div className='relative shrink-0 w-full h-[400px]'>
-              <img
-                src={item.url}
-                alt={item.title}
-                className='w-full h-full object-cover rounded-lg transition-transform duration-300 group-hover:scale-105'
-                draggable={false}
-              />
-              {item.hotspots?.map((hotspot, hotspotIndex) => (
-                <Hotspot
-                  key={hotspotIndex}
-                  x={hotspot.x}
-                  y={hotspot.y}
-                  title={hotspot.title}
-                  description={hotspot.description}
-                />
-              ))}
-            </div>
-          </div>
-        </DialogTrigger>
-        <DialogContent className="sm:max-w-[80vw] p-0 border-0 bg-transparent">
+      <div className='relative overflow-hidden rounded-lg'>
+        <div className='relative shrink-0 w-full h-[400px]'>
           <img
             src={item.url}
             alt={item.title}
-            className='w-full h-auto max-h-[90vh] object-contain rounded-lg'
+            className='w-full h-full object-cover rounded-lg'
+            draggable={false}
           />
-        </DialogContent>
-      </Dialog>
+          {item.hotspots?.map((hotspot, hotspotIndex) => (
+            <Hotspot
+              key={hotspotIndex}
+              x={hotspot.x}
+              y={hotspot.y}
+              title={hotspot.title}
+              description={hotspot.description}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
