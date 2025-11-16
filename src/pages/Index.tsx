4b2781c751704690a9338274e3f85cc81a815d73
[dyad] <khadeem.smart@outlook.com>
2025-11-16 14:01:48 +0100

    [dyad] Updating hero video to nightclub scene - wrote 1 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -6,8 +6,8 @@ import { useTranslation } from "react-i18next";
 
 const Index = () => {
   const { t } = useTranslation();
-  const videoSrc = 'https://videos.pexels.com/video/857251/download';
-  const posterSrc = 'https://images.pexels.com/photos/1287145/pexels-photo-1287145.jpeg';
+  const videoSrc = 'https://videos.pexels.com/video-files/8899388/8899388-hd_1920_1080_25fps.mp4';
+  const posterSrc = 'https://images.pexels.com/videos/8899388/pexels-photo-8899388.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500';
 
   return (
     <>
@@ -18,6 +18,7 @@ const Index = () => {
           autoPlay
           loop
           muted
+          playsInline
           className="absolute z-0 w-auto min-w-full min-h-full max-w-none"
         >
           Your browser does not support the video tag.
