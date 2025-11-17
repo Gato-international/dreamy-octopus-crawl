1dfab0b9d8dce31a5991eeaa7adccb0e9ad3483f
[dyad] <khadeem.smart@outlook.com>
2025-11-18 00:23:00 +0100

    [dyad] Fixing a component import error - wrote 1 file(s)

--- src/pages/CommentsExport.tsx
+++ src/pages/CommentsExport.tsx
@@ -3,7 +3,7 @@ import { supabase } from '@/integrations/supabase/client';
 import { Button } from '@/components/ui/button';
 import { showSuccess } from '@/utils/toast';
 import { Helmet } from 'react-helmet-async';
-import DOMPurify from 'dompurify';
+import * as DOMPurify from 'dompurify';
 
 type Comment = {
   id: string;
